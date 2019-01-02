30c9762fe2a089d4bc7cd6791316e778d4612cb8
Amandeep Singh <as.smartdevelopers@gmail.com>
2019-01-02 22:58:45 +0530

    Updated browser

--- install/fullscreen.sh
+++ install/fullscreen.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 screen -d -m -S expressApp bash -c 'cd /home/pi/expressApp && npm start > output.txt'
 sleep 10;
-midori -e Fullscreen -a http://localhost:3000
+unclutter &
+matchbox-window-manager &
+iceweasel http://localhost:3000 --display=:0 &
+sleep 15s;
+xte "key F11" -x:0
+#midori -e Fullscreen -a http://localhost:3000
