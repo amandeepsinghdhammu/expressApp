ba06c315dd314c76a5b100f793806e85aa8922f0
Amandeep Singh <as.smartdevelopers@gmail.com>
2019-01-06 20:23:47 +0530

    Fixed HTML issue

--- install/fullscreen.sh
+++ install/fullscreen.sh
@@ -2,8 +2,8 @@
 screen -d -m -S expressApp bash -c 'cd /home/pi/expressApp && npm start > output.txt'
 sleep 10;
 unclutter &
-matchbox-window-manager &
-iceweasel http://localhost:3000 --display=:0 &
-sleep 15s;
-xte "key F11" -x:0
-#midori -e Fullscreen -a http://localhost:3000
+#matchbox-window-manager &
+#iceweasel http://localhost:3000 --display=:0 &
+#sleep 15s;
+#xte "key F11" -x:0
+midori -e Fullscreen -a http://localhost:3000
